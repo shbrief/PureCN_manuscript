1770b9cd19a9b6951260a27941cca11405973afb
Sehyun Oh <shbrief@hotmail.com>
2019-12-09 21:03:30 -0500

    reorganize dNdSCV folder

---
+++ Revision/dNdSCV/script/rundNdSCV.R
@@ -0,0 +1,18 @@
+# Run dNdSCV 
+# [input] snv_list : a list of data frames with 5 columns for dNdScv ("sampleID", "chr", "pos", "ref", "mut")
+# [output] dndsout : a list of dNdSCV outputs
+
+wd = "~/data2/PureCN_manuscript/Revision/dNdSCV"
+dndsout_tumor = list()
+
+for (i in seq_along(snv_list)) {
+    tryCatch({
+        sapmle_name = names(snv_list[i])
+        dndsout = dndscv(snv_list[[i]], 
+                         refdb = file.path(wd, "GRCh38_refcds.rda"),
+                         # max_muts_per_gene_per_sample = 5,   # default is 3
+                         # max_coding_muts_per_sample = 20000,   # default is 3000
+                         cv = NULL)
+        dndsout_tumor[[sample_name]] = dndsout
+    }, error = function(e) {cat("ERROR with ", paste0("sample #",i, "of", sample_name), "\n")})
+}
