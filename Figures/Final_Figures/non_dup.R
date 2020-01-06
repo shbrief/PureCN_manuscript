5e5f9bdc1fbb316656695cc853bb7b0c12b4648a
Sehyun Oh <shbrief@hotmail.com>
2020-01-06 20:38:17 +0000

    figures in pdf

--- Figures/Final_Figures/non_dup.R
+++ Figures/Final_Figures/non_dup.R
@@ -1,6 +1,6 @@
 # 236 OVC samples
-ovc_236 = read.csv("~/wallabe4_backup/github/PureCN_manuscript/Results/duplication/ovc_236.csv")[,2]
+ovc_236 = read.csv("/data2/PureCN_manuscript/Results/duplication/ovc_236.csv")[,2]
 
 # 442 LUAD samples
-luad_442 = read.csv("~/wallabe4_backup/github/PureCN_manuscript/luad/Results/duplication/luad_442.csv")[,2]
+luad_442 = read.csv("/data2/PureCN_manuscript/luad/Results/duplication/luad_442.csv")[,2]
 luad_442 = stringr::str_extract(luad_442, "TCGA.{11}")
